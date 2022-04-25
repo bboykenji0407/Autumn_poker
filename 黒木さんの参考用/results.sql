f9e9209c294261307f08e8115872f9e74b4dc84a
kenji_kimura <bboy.kenji.flex@gmail.com>
2022-04-25 13:23:09 +0900

    黒木さんの各テーブルのインポート

--- 黒木さんの参考用/results.sql
+++ 黒木さんの参考用/results.sql
@@ -6,27 +6,27 @@
 -- 生成日時: 2022 年 4 月 22 日 13:05
 -- サーバのバージョン： 5.7.34
 -- PHP のバージョン: 7.4.21
-​
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-​
-​
+
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-​
+
 --
 -- データベース: `autumn_curriculum`
 --
-​
+
 -- --------------------------------------------------------
-​
+
 --
 -- テーブルの構造 `results`
 --
-​
+
 CREATE TABLE `results` (
   `id` int(11) NOT NULL,
   `win` int(11) NOT NULL DEFAULT '0',
@@ -34,18 +34,18 @@ CREATE TABLE `results` (
   `create_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `update_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-​
+
 --
 -- テーブルのデータのダンプ `results`
 --
-​
+
 INSERT INTO `results` (`id`, `win`, `lose`, `create_timestamp`, `update_timestamp`) VALUES
 (1, 3, 5, '2022-04-04 22:33:37', '2022-04-17 06:44:14');
-​
+
 --
 -- ダンプしたテーブルのインデックス
 --
-​
+
 --
 -- テーブルのインデックス `results`
 --
@@ -53,18 +53,18 @@ ALTER TABLE `results`
   ADD PRIMARY KEY (`id`),
   ADD KEY `create_timestamp` (`create_timestamp`),
   ADD KEY `update_timestamp` (`update_timestamp`);
-​
+
 --
 -- ダンプしたテーブルの AUTO_INCREMENT
 --
-​
+
 --
 -- テーブルの AUTO_INCREMENT `results`
 --
 ALTER TABLE `results`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
-​
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
